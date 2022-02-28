c996cef527f6b2e8d0b76ef3b878a81465738653
彭一天 <66310292+GlitteringAu@users.noreply.github.com>
2022-02-28 21:25:29 +0800

    Add files via upload

--- synthesizer.py
+++ synthesizer.py
@@ -6,7 +6,10 @@ import hparams
 import numpy as np
 import os
 
+import torchsnooper
 
+
+# @torchsnooper.snoop()
 def synthesizer(model, text, device):
     seq = text_to_sequence(text, [hparams.cleaners])
     # seq = torch.Tensor(seq).to(device)
@@ -15,23 +18,23 @@ def synthesizer(model, text, device):
         seq = torch.from_numpy(seq).type(torch.cuda.LongTensor).to(device)
     else:
         seq = torch.from_numpy(seq).type(torch.LongTensor).to(device)
-    # print(seq)
+    # print('-----------------------------------------', seq.size())  # [1, 25]
 
     # Provide [GO] Frame
     mel_input = np.zeros(
         [np.shape(seq)[0], hparams.num_mels, 1], dtype=np.float32)
     mel_input = torch.Tensor(mel_input).to(device)
-    # print(np.shape(mel_input))
-
+    # print('-----------------------------------------', np.shape(mel_input))  # [1, 80, 1]
+    # print('===========================', np.shape(seq)) # [1, 25]
     model.eval()
     with torch.no_grad():
-        _, linear_output = model(seq, mel_input)
-        # print(np.shape(linear_output))
+        _, linear_output, mu, log_var = model(seq, mel_input)
+        # print('-----------------------------------------', np.shape(linear_output))
 
     # trans_linear = audio.trans(linear_output[0].cpu().numpy())
     wav = audio.inv_spectrogram(linear_output[0].cpu().numpy())
     # print(audio.find_endpoint(wav))
-    # print(np.shape(wav))
+    # print('-----------------------------------------', np.shape(wav))
     wav = wav[:audio.find_endpoint(wav)]
     # print(np.shape(wav))
     return wav
@@ -41,15 +44,19 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Define model
-    model = Tacotron().to(device)
+    # model = Tacotron().to(device)
+    model = nn.DataParallel(Tacotron()).to(device)
     print("Model Have Been Defined")
 
     # Load checkpoint
     checkpoint = torch.load(os.path.join(
-        hparams.checkpoint_path, 'checkpoint_20500.pth.tar'))
+        hparams.checkpoint_path, 'checkpoint_39000.pth.tar'))
     model.load_state_dict(checkpoint['model'])
+    # model.eval()
     print("Load Done")
 
-    text = "I am very happy to see you again."
+    # text = "I thought you meant how old are you"
+    # text = "This used to be Jerry's occupation"
+    text = "Printing, then, for our purpose, may be considered as the art of making books by means of movable types."
     wav = synthesizer(model, text, device)
-    audio.save_wav(wav, text + ".wav")
+    audio.save_wav(wav, './result/' + "39000" + text + "_.wav")
